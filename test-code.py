22e8afb6c1223115f0d31ef6dfca903a4c3ebdea
tyler long <tyler.derek.long@gmail.com>
2025-04-12 10:45:11 -0500

    Added logic to account for plural, singular, and no more

--- test-code.py
+++ test-code.py
@@ -2,11 +2,21 @@
 
 import time
 
-bottlesOfBeer = 99
+bottlesOfBeer = 5
 
 while bottlesOfBeer > 0:
-    print(f"{bottlesOfBeer} bottles of beer on the wall, {bottlesOfBeer} bottles of beer! Take one down, pass it around...")
-    bottlesOfBeer -= 1
-    print(f"{bottlesOfBeer} bottles of beer on the wall!")
-    print("\n")
-    time.sleep(.5)
+    if bottlesOfBeer >=2:
+        print(f"{bottlesOfBeer} bottles of beer on the wall, {bottlesOfBeer} bottles of beer! Take one down, pass it around...")
+        bottlesOfBeer -= 1
+        if bottlesOfBeer == 1:
+            print(f"{bottlesOfBeer} bottle of beer on the wall!")
+            print("\n")
+            time.sleep(.5)
+            continue
+        print(f"{bottlesOfBeer} bottles of beer on the wall!")
+        print("\n")
+        time.sleep(.5)
+    elif bottlesOfBeer == 1:
+        print(f"{bottlesOfBeer} bottle of beer on the wall, {bottlesOfBeer} bottle of beer! Take one down, pass it around...")
+        bottlesOfBeer -= 1
+        print(print(f"no more bottles of beer on the wall!"))
